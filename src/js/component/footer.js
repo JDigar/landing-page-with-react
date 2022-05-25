49fcd43471efd5974a549429cfdd799f00b45957
JDigar <jdigar96@gmail.com>
2022-05-25 19:37:53 +0000

    finished

---
+++ src/js/component/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div>
+			<footer className="text-center p-4">
+				<div>
+					<p>Esto es el Footer</p>
+				</div>
+			</footer>
+		</div>
+	);
+};
+
+export default Footer;
